2651d694f22a9fd2d9fbf957917c805220c7af7c
Marcel Morin <marcel.morin@mail.mcgill.ca>
2019-06-05 00:37:11 -0500

    log to new file every new day
    Need to update to every month after fully debugged & when I figure out how to send user input via serial without blocking on user input...

--- serialLog.py
+++ serialLog.py
@@ -1,15 +1,48 @@
 import serial
+import logging
+import re
+from datetime import datetime
 
 ser = serial.Serial('/dev/ttyACM0',9600)
 
-#TODO: parse serial readline to get sensor reading
-#      then get timestamp and log both to spreadsheet in sambaShare
+#TODO: log to different files each month, right now setup for logging each day
 
 #TODO: allow for user to send commands to Arduino
+
+formatter = logging.Formatter('%(message)s')
+currentDay = datetime.now().day
+
+def setup_logger(name, log_file, level=logging.INFO):
+
+    handler = logging.FileHandler(log_file)        
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+    logger.info('date time data')
+
+    return logger
+
+logger = setup_logger(datetime.now().strftime('%Y-%m-%d'), datetime.now().strftime('%Y-%m-%d')+'.csv')
+
 
 while 1:
 
     if(ser.in_waiting > 0):
         line = ser.readline()
-        print(line)
+        #read user input if there is any... (do not block on read)
+        #send command via serial
+        
+        #check time to see if you need to write to a different file
+        if(currentDay < datetime.now().day or (currentDay==30 and datetime.now().day == 1)):
+            logger = setup_logger(datetime.now().strftime('%Y-%m-%d'), datetime.now().strftime('%Y-%m-%d')+'.csv')
+            currentDay = datetime.now().day
+            
+        stringLine = re.split(" |\r",str(line, 'utf-8')) #convert serial data from bytes to utf-8 & split string
+        print(stringLine)
+        if(stringLine[1] == "data:"): #only log if it is data and not a command response
+            logger.info(datetime.now().strftime('%Y-%m-%d %H:%M:%S ') + stringLine[2] +' '+ stringLine[3])
+            print(datetime.now().strftime('%Y-%m-%d %H:%M:%S ') + stringLine[2] +' '+ stringLine[3]);
+            #print(line)
  
