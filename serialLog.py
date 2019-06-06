f380187aacceb30a5fd86ecee0920b1ab8e29ed7
marcypm <marcel.morin@mail.mcgill.ca>
2019-06-05 19:58:31 -0500

    Add cmd display
    Debugged and added commands for user

--- serialLog.py
+++ serialLog.py
@@ -8,7 +8,6 @@ ser = serial.Serial('/dev/ttyACM0',9600)
 
 #TODO: log to different files each month, right now setup for logging each day
 
-#TODO: allow for user to send commands to Arduino
 class InputTimedOut(Exception):
     pass
 
@@ -42,16 +41,35 @@ def setup_logger(name, log_file, level=logging.INFO):
 
     return logger
 
-logger = setup_logger(datetime.now().strftime('%Y-%m-%d'), datetime.now().strftime('%Y-%m-%d')+'.csv')
+def printCommands():
+    print("   disp info\t (toggle serial info in shell)")
+    print("   default\t (change system settings back to default)")
+    print("   poll\t\t (bring system into polling state)")
+    print("   water\t (force watering)")
+    print("   stop\t\t (stop polling data and just idle)\n")
+    print("   sample x\t (change sampling rate to [x])")
+    print("   thresh x\t (change watering threshold to [x]%)")
+    print("   watertime x\t (change watering time to [x])")
+    
 
-#TODO: display command to see all available commands
+logger = setup_logger(datetime.now().strftime('%Y-%m-%d'), datetime.now().strftime('%Y-%m-%d')+'.csv')
+info = 0
+
 while 1:
 
     command = checkInput(timeout=0.1)
     if (command !=""):
         print ("sent: "+command) #send serially
-        string_encode = command.encode()
-        ser.write(string_encode)
+        if(command == "cmd" or command == "commands" or command == "command"):
+            printCommands()
+        elif(command == "disp info"):
+            if(info == 0):
+                info = 1
+            else:
+                info = 0
+        else:
+            string_encode = command.encode()
+            ser.write(string_encode)
 
     if(ser.in_waiting > 0):
         line = ser.readline()
@@ -66,13 +84,13 @@ while 1:
             
         stringLine = re.split(" |\r",str(line,'utf-8')) #convert serial data from bytes to utf-8 & split string
         #stringLine = re.split(" |\r",str(line.encode('utf-8'))) #python2 equivalent
-        print(stringLine)
+        #print(stringLine)
         if(stringLine[1] == "data:"): #only log if it is data and not a command response
             logger.info(datetime.now().strftime('%Y-%m-%d %H:%M:%S ') + stringLine[2] +' '+ stringLine[3])
             #print(datetime.now().strftime('%Y-%m-%d %H:%M:%S ') + stringLine[2] +' '+ stringLine[3]);
-            #print(line)
-        else:
-            print(line.decode("utf-8"))
+            if(info==1):print(line.decode("utf-8"))
+        #else:
+            #print(line.decode("utf-8"))
                 
         
             
